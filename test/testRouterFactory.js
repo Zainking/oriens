88f672d727ead6c9a32acf4f931546dc6abd409a
Zain <wangziyi.2019@bytedance.com>
2019-06-12 20:14:03 +0800

    Add simple data and template files

--- test/testRouterFactory.js
+++ test/testRouterFactory.js
@@ -1,14 +1,13 @@
 const mergeRouterAndData = require('../lib/routerFactory').mergeRouterAndData
 
 const res = mergeRouterAndData({
-    "/": ["templateName", { "__data": "./simple/data/1.json" }],
-    "/[id]/[id2]/[id3]": [
+    "/": ["templateName", { "__data": "./simple/data/d.json" }],
+    "/[id]/[id2]": [
         "templateName",
         {
             "id": "./simple/data/a",
-            "id2": "./simple/data/a",
-            "id3": "./simple/data/a",
-            "__data": "./simple/data/1.json"
+            "id2": "./simple/data/b",
+            "__data": "./simple/data/c.json"
         }
     ]
 })
--- test/testTemplateFactory.js
+++ test/testTemplateFactory.js
@@ -1,4 +1,4 @@
 const createTemplate = require('../lib/templateFactory').createTemplate
 
-const tpl = createTemplate('./simple/templates/a/test.pug', ['./simple/templates/a/1.styl', './simple/templates/a/2.styl'])
-tpl.complie('./dist', { data: 'hello' })
+const tpl = createTemplate('./simple/templates/a/a.pug', ['./simple/templates/a/a1.styl', './simple/templates/a/a2.styl'])
+tpl.complie('./dist')
